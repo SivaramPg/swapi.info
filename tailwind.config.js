33996a63e8f3d8788a008ce0c125397dea1a6ebb
SivaramPg <sivarampg95@gmail.com>
2023-08-06 13:24:58 +0530

    cleanup and font updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,10 +9,9 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--font-mulish)', ...fontFamily.sans],
+        mono: ['var(--font-space-mono)', ...fontFamily.mono],
       },
     },
   },
