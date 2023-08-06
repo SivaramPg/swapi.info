5a12682171f342885b3e95fb8172318e28c4d9a6
SivaramPg <sivarampg95@gmail.com>
2023-08-06 19:47:01 +0530

    UI updates

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,5 +1,8 @@
 import fsPromises from 'fs/promises'
 import path from 'path'
+import clsx from 'clsx'
+import Link from 'next/link'
+import Image from 'next/image'
 
 import ResponseDisplayElement from '@/components/ResponseDisplayElement'
 import ApiEndpointElement from '@/components/ApiEndpointElement'
@@ -41,6 +44,31 @@ export default async function Page({
     <main className="container mx-auto min-h-[calc(100vh-64px)] py-20 flex flex-col gap-8 items-center justify-center">
       <h1 className="text-7xl font-black mb-10">/{category}</h1>
       <Breadcrumbs pathElements={[category]} />
+      <div className={clsx('w-full max-w-screen-lg flex flex-col gap-2')}>
+        <h4 className="font-bold text-xl opacity-70">All {category}:</h4>
+        <div className="flex flex-wrap gap-4">
+          {data.map((obj: any, i: number) => (
+            <Link
+              key={i}
+              href={obj.url.replace('/api', '')}
+              className="w-1/4 grow border-2 rounded-lg shadow-sm bg-blue-50"
+            >
+              <div className="w-full h-fit p-4 flex items-center justify-between gap-2">
+                <h2 className="font-bold text-lg capitalize">
+                  {obj.url.split('/').at(-1).split('.')[0]}.{' '}
+                  {obj.title ?? obj.name}
+                </h2>
+                <Image
+                  src="/icons/caret-right.svg"
+                  alt="..."
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
       <ApiEndpointElement
         text={`https://sw-api.sivaramp.com/api/${category}`}
       />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import fsPromises from 'fs/promises'
 import path from 'path'
+import Image from 'next/image'
+import Link from 'next/link'
 
 import ResponseDisplayElement from '@/components/ResponseDisplayElement'
 import ApiEndpointElement from '@/components/ApiEndpointElement'
+import clsx from 'clsx'
 
 async function getRootJson() {
   const jsonFile = await fsPromises.readFile(
@@ -18,6 +21,28 @@ export default async function Home() {
   return (
     <main className="container mx-auto min-h-[calc(100vh-64px)] py-20 flex flex-col gap-8 items-center justify-center">
       <h1 className="text-7xl font-black mb-10">SW-API</h1>
+      <div className={clsx('w-full max-w-screen-lg flex flex-col gap-2')}>
+        <h4 className="font-bold text-xl opacity-70">All Categories:</h4>
+        <div className="flex flex-wrap gap-4">
+          {Object.entries(data).map(([key, value], i: number) => (
+            <Link
+              key={i}
+              href={(value as string).replace('/api', '')}
+              className="w-1/4 grow border-2 rounded-lg shadow-sm bg-blue-50"
+            >
+              <div className="w-full h-fit p-4 flex items-center justify-between gap-2">
+                <h2 className="font-bold text-lg capitalize">{key}</h2>
+                <Image
+                  src="/icons/caret-right.svg"
+                  alt="..."
+                  width={24}
+                  height={24}
+                />
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
       <ApiEndpointElement text={`https://sw-api.sivaramp.com/api/`} />
       <ResponseDisplayElement>
         {JSON.stringify(data, null, 4)}
