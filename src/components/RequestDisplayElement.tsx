646f239223c2318fa59466b82b3deb83ef934655
SivaramPg <sivarampg95@gmail.com>
2024-03-20 10:27:56 +0530

    fix request

--- src/components/RequestDisplayElement.tsx
+++ src/components/RequestDisplayElement.tsx
@@ -33,5 +33,5 @@ function getRequest(slug: string) {
   return `fetch("https://swapi.info/api${slug}")
     .then((res) => res.json()) // Parse the JSON content from the API to be consumed
     .then((json) => console.log(json)) // Log the JSON response to your console
-    .catch(error) => console.error(error) // Log the API error (if any) to your console`
+    .catch((error) => console.error(error)) // Log the API error (if any) to your console`
 }
