81ded95d59c6395a1ea1c4fed194ec9bb67360d3
SivaramPg <dev.sivaramp@gmail.com>
2025-05-01 20:33:31 +0530

    feat: Enable manual page leave capture in PostHogProvider for enhanced analytics tracking

--- src/components/posthog-provider.tsx
+++ src/components/posthog-provider.tsx
@@ -14,6 +14,7 @@ export function PostHogProvider({ children }: { children: React.ReactNode }) {
 				process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
 			person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
 			capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+			capture_pageleave: true,
 		})
 	}, [])
 
