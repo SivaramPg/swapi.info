242ae142cd92da6aac3d37c47297e2cfaf5a789e
SivaramPg <dev.sivaramp@gmail.com>
2025-05-19 21:26:00 +0530

    chore: Update Tailwind CSS configuration to include typography and animation plugins, and modify font settings

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+import typography from "@tailwindcss/typography"
 import { fontFamily } from "tailwindcss/defaultTheme"
 
 /** @type {import('tailwindcss').Config} */
@@ -12,9 +13,68 @@ export default {
 		extend: {
 			fontFamily: {
 				sans: ["var(--font-bricolage)", ...fontFamily.sans],
-				mono: ["var(--font-azeret-mono)", ...fontFamily.mono],
+				mono: ["var(--font-martian-mono)", ...fontFamily.mono],
+			},
+			typography: (theme) => ({
+				DEFAULT: {
+					css: {
+						pre: {
+							backgroundColor: theme("colors.neutral.800"),
+							paddingTop: theme("spacing.3"),
+							paddingBottom: theme("spacing.3"),
+							paddingLeft: theme("spacing.4"),
+							paddingRight: theme("spacing.4"),
+							borderRadius: theme("borderRadius.md"),
+						},
+						"code::before": {
+							content: '""',
+						},
+						"code::after": {
+							content: '""',
+						},
+						code: {
+							backgroundColor: theme("colors.neutral.800"),
+							padding: "0.2em 0.4em",
+							borderRadius: theme("borderRadius.sm"),
+							fontWeight: "500",
+							color: theme("colors.yellow.300"),
+						},
+						a: {
+							color: theme("colors.yellow.400"),
+							"&:hover": {
+								color: theme("colors.yellow.300"),
+							},
+						},
+					},
+				},
+				invert: {
+					css: {
+						pre: {
+							backgroundColor: theme("colors.neutral.800"),
+						},
+						code: {
+							backgroundColor: theme("colors.neutral.700"),
+							color: theme("colors.yellow.300"),
+						},
+						a: {
+							color: theme("colors.yellow.400"),
+							"&:hover": {
+								color: theme("colors.yellow.300"),
+							},
+						},
+					},
+				},
+			}),
+			animation: {
+				marquee: "marquee 25s linear infinite",
+			},
+			keyframes: {
+				marquee: {
+					"0%": { transform: "translateX(0%)" },
+					"100%": { transform: "translateX(-50%)" },
+				},
 			},
 		},
 	},
-	plugins: [],
+	plugins: [typography, require("tailwindcss-animate")],
 }
