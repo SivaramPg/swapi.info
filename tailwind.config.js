d699ddb80a1113294d9c00b5e6ad7318f4e90115
SivaramPg <dev.sivaramp@gmail.com>
2025-05-24 18:07:23 +0530

    chore: Remove marquee animation and keyframes from Tailwind CSS configuration for cleaner styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -65,15 +65,6 @@ export default {
 					},
 				},
 			}),
-			animation: {
-				marquee: "marquee 25s linear infinite",
-			},
-			keyframes: {
-				marquee: {
-					"0%": { transform: "translateX(0%)" },
-					"100%": { transform: "translateX(-50%)" },
-				},
-			},
 		},
 	},
 	plugins: [typography, require("tailwindcss-animate")],
